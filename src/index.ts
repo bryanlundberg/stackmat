52935e244f41fc94a2aea811eda978dad41dc91b
Bryan Lundberg <bryanlundberg@outlook.com>
2025-11-07 06:05:30 -0600

    Revert "refactor: update export structure in index.ts"
    This reverts commit e583c897b0f2f6f4b7b802e3a4e90c77e520c4d4.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { Stackmat } from './stackmat'
+export { default, default as Stackmat } from './stackmat'
 export { TimerEvent } from './event/timer-event'
 export { TimerEventHandler } from './event/timer-event-manager'
 export { Packet, PacketStatus } from './packet/packet'

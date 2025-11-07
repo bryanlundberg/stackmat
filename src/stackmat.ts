25d0ba1b63bc39a729bbb9c78ae65d8ea200d5d1
Bryan Lundberg <bryanlundberg@outlook.com>
2025-11-07 06:05:13 -0600

    Revert "refactor: update Stackmat export to named export"
    This reverts commit aec2c5adbd809a1582a463fd23afe8284c347735.

--- src/stackmat.ts
+++ src/stackmat.ts
@@ -3,7 +3,7 @@ import { isEvent, TimerEvent } from './event/timer-event'
 import { TimerEventHandler, TimerEventManager } from './event/timer-event-manager'
 import { Packet } from './packet/packet'
 
-export class Stackmat {
+export default class Stackmat {
     private eventManager: TimerEventManager = new TimerEventManager()
     private audioProcessor?: AudioProcessor
 
